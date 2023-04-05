d302cc2fde5303ee5c33a7d56eaf0b985af92cfa
AAelduob <aaelduob@gmail.com>
2023-04-05 19:04:26 -0400

    Made a cart count on the icon

--- main.js
+++ main.js
@@ -1,73 +1,17 @@
-//
-//  JS File
-//  You may remove the code below - it's just boilerplate
-//
+let cartCount = 0;
+const cartCountSpan = document.getElementById('cart-count');
 
-//
-// Variables
-//
+const addToCartButtons = document.querySelectorAll('.addToCart');
 
-// Constants
-const appID = "app";
-const headingText = "Develop. Preview. Ship.";
-const headingTextIcon = "🚀";
-const projectDueDate = "11 April 2023 11:59";
+addToCartButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    cartCount++;
+    cartCountSpan.textContent = cartCount;
 
-// Variables
-let countdownDate = new Date(projectDueDate);
-
-// DOM Elements
-let appContainer = document.getElementById(appID);
-
-//
-// Functions
-//
-
-function calculateDaysLeft(countdownDate) {
-  const now = new Date().getTime();
-  const countdown = new Date(countdownDate).getTime();
-
-  const difference = (countdown - now) / 1000;
-
-  // Countdown passed already
-  if (difference < 1) {
-    return null;
-  }
-
-  const days = Math.floor(difference / (60 * 60 * 24));
-
-  return days;
-}
-
-// Add a heading to the app container
-function inititialise() {
-  // If anything is wrong with the app container then end
-  if (!appContainer) {
-    console.error("Error: Could not find app contianer");
-    return;
-  }
-
-  // Create an h1 and add it to our app
-  const h1 = document.createElement("h1");
-  const daysLeft = calculateDaysLeft(countdownDate);
-  let headingTextCalculated = headingText;
-
-  if (daysLeft) {
-    headingTextCalculated = headingTextCalculated.concat(
-      " In ",
-      daysLeft.toString(),
-      " days "
-    );
-  }
-  h1.textContent = headingTextCalculated.concat(headingTextIcon);
-  appContainer.appendChild(h1);
-
-  // Init complete
-  console.log("App successfully initialised");
-}
-
-//
-// Inits & Event Listeners
-//
-
-inititialise();
+    if (cartCount !== 0) {
+      cartCountSpan.style.display = 'inline-block';
+    } else {
+      cartCountSpan.style.display = 'none';
+    }
+  });
+});
