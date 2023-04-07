68c304a66073906144e57175d5911a06ada0341e
Conall Nelligan <conallnelligan@gmail.com>
2023-04-07 17:45:31 -0400

    Made store page fully responisve

--- main.js
+++ main.js
@@ -1,73 +1,37 @@
-//
-//  JS File
-//  You may remove the code below - it's just boilerplate
-//
+//Collapsing navbar
 
-//
-// Variables
-//
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
 
-// Constants
-const appID = "app";
-const headingText = "Develop. Preview. Ship.";
-const headingTextIcon = "🚀";
-const projectDueDate = "11 April 2023 11:59";
-
-// Variables
-let countdownDate = new Date(projectDueDate);
-
-// DOM Elements
-let appContainer = document.getElementById(appID);
-
-//
-// Functions
-//
-
-function calculateDaysLeft(countdownDate) {
-  const now = new Date().getTime();
-  const countdown = new Date(countdownDate).getTime();
-
-  const difference = (countdown - now) / 1000;
-
-  // Countdown passed already
-  if (difference < 1) {
-    return null;
-  }
-
-  const days = Math.floor(difference / (60 * 60 * 24));
-
-  return days;
+if (bar) {
+  bar.addEventListener('click', () => {
+    nav.classList.add('active');
+  })
 }
 
-// Add a heading to the app container
-function inititialise() {
-  // If anything is wrong with the app container then end
-  if (!appContainer) {
-    console.error("Error: Could not find app contianer");
-    return;
-  }
-
-  // Create an h1 and add it to our app
-  const h1 = document.createElement("h1");
-  const daysLeft = calculateDaysLeft(countdownDate);
-  let headingTextCalculated = headingText;
-
-  if (daysLeft) {
-    headingTextCalculated = headingTextCalculated.concat(
-      " In ",
-      daysLeft.toString(),
-      " days "
-    );
-  }
-  h1.textContent = headingTextCalculated.concat(headingTextIcon);
-  appContainer.appendChild(h1);
-
-  // Init complete
-  console.log("App successfully initialised");
+if (close) {
+  close.addEventListener('click', () => {
+    nav.classList.remove('active');
+  })
 }
 
-//
-// Inits & Event Listeners
-//
+//cart counter
 
-inititialise();
+let cartCount = 0;
+const cartCountSpan = document.getElementById('cart-count');
+
+const addToCartButtons = document.querySelectorAll('.addToCart');
+
+addToCartButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    cartCount++;
+    cartCountSpan.textContent = cartCount;
+
+    if (cartCount !== 0) {
+      cartCountSpan.style.display = 'inline-block';
+    } else {
+      cartCountSpan.style.display = 'none';
+    }
+  });
+});
