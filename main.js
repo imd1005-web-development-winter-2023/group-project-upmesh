eb6c2be65dbb005a2366ea07635ef3f301412ea5
AAelduob <aaelduob@gmail.com>
2023-04-08 21:41:48 -0400

    cart code

--- main.js
+++ main.js
@@ -16,17 +16,28 @@ if (close) {
   })
 }
 
-//cart counter
+//cart
 
-let cartCount = 0;
+if (window.location.pathname === '/index.html') {
+  localStorage.removeItem('cartCount');
+  localStorage.removeItem('cartItems');
+}
+
+let cartCount = parseInt(localStorage.getItem('cartCount')) || 0;
 const cartCountSpan = document.getElementById('cart-count');
+const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
 
 const addToCartButtons = document.querySelectorAll('.addToCart');
 
 addToCartButtons.forEach(button => {
   button.addEventListener('click', () => {
+    const card = button.closest('.card');
     cartCount++;
+    localStorage.setItem('cartCount', cartCount);
     cartCountSpan.textContent = cartCount;
+
+    cartItems.push(card);
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
 
     if (cartCount !== 0) {
       cartCountSpan.style.display = 'inline-block';
@@ -35,3 +46,31 @@ addToCartButtons.forEach(button => {
     }
   });
 });
+
+function displayCartItems() {
+  const cartItemsElement = document.getElementById('cart-items');
+  cartItemsElement.innerHTML = '';
+
+  cartItems.forEach(item => {
+    const itemElement = document.createElement('div');
+    itemElement.classList.add('cart-item');
+
+    const itemName = item.querySelector('.card-title').textContent;
+    const itemPrice = item.querySelector('.card-price').textContent;
+
+    const nameElement = document.createElement('h2');
+    nameElement.textContent = itemName;
+
+    const priceElement = document.createElement('p');
+    priceElement.textContent = itemPrice;
+
+    itemElement.appendChild(nameElement);
+    itemElement.appendChild(priceElement);
+
+    cartItemsElement.appendChild(itemElement);
+  });
+
+  document.getElementById('cart-count').textContent = cartCount;
+}
+
+displayCartItems();
